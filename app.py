b8ec20670aa503c64ae51f67542ecf61afbc2cc3
Jesus Graterol <96505970+YzusG@users.noreply.github.com>
2024-09-17 12:32:12 -0400

    Update app.py
    Comentarios

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
+'''
+Trabajo para Practicar
+Prendas Personalizadas Paola's
+
+Prenda:
+    Codigo: Int
+    Talla: Texto
+    Decoracion: Texto
+    Entrega: texto
+    Precio: Float
+'''
+
+# Importaciones
 from funciones.menu import menu
 
 
-
+# Programa Principal
 menu()
